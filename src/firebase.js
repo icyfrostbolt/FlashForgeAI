7076d3dea95b2ef1db136196410a5bac77ab5fe3
TERMIN20 <58315733+TERMIN20@users.noreply.github.com>
2025-01-19 00:41:27 -0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,31 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
-
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
-  apiKey: "AIzaSyDo9sC6z4Anum-bOUyzO6BzKzGBpjgCSfE",
-
-  authDomain: "flashforge-2d6ec.firebaseapp.com",
-
-  projectId: "flashforge-2d6ec",
-
-  storageBucket: "flashforge-2d6ec.firebasestorage.app",
-
-  messagingSenderId: "552754710121",
-
-  appId: "1:552754710121:web:1235b821a9b73797c104f8",
-
-  measurementId: "G-B170SETTZY"
-
+  apiKey: "AIzaSyAy5k6o5ADegj6DtseQTQei0tRTsHk3DI4",
+  authDomain: "flashforge-7c12f.firebaseapp.com",
+  projectId: "flashforge-7c12f",
+  storageBucket: "flashforge-7c12f.firebasestorage.app",
+  messagingSenderId: "344139418819",
+  appId: "1:344139418819:web:4c8c14d29f1dc35f9a2676",
+  measurementId: "G-3HHLYLNH5V"
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const analytics = getAnalytics(app);

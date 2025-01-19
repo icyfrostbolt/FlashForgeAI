6c7238d4119d3d062f9422a169888f96e01c03c2
TERMIN20 <58315733+TERMIN20@users.noreply.github.com>
2025-01-18 17:11:09 -0800

    Update firebase.js
    the firebase shit

--- src/firebase.js
+++ src/firebase.js
@@ -8,14 +8,23 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: ".firebaseapp.com",
-  projectId: "anonymous-qa",
-  storageBucket: "anonymous-qa.firebasestorage.app",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+
+  apiKey: "AIzaSyDo9sC6z4Anum-bOUyzO6BzKzGBpjgCSfE",
+
+  authDomain: "flashforge-2d6ec.firebaseapp.com",
+
+  projectId: "flashforge-2d6ec",
+
+  storageBucket: "flashforge-2d6ec.firebasestorage.app",
+
+  messagingSenderId: "552754710121",
+
+  appId: "1:552754710121:web:1235b821a9b73797c104f8",
+
+  measurementId: "G-B170SETTZY"
+
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
